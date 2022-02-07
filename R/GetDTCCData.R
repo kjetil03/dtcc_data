7479bcae3da053a887f144b80b206a7cdf1b8ed8
Stiansen <Kjetil.Stiansen@Norges-Bank.no>
2022-02-07 10:11:58 +0100

    Flytta beregning av EstIssueDate til Sql

--- R/GetDTCCData.R
+++ R/GetDTCCData.R
@@ -19,7 +19,29 @@
 GetDTCCData <- function() {
   
 
-  sql_srch = "SELECT CUSIP,ProductType,IssuerName,IssueDate,SettlementDate,MaturityDate,InterestRateType,IncomePaymentType,
+  sql_srch = "
+
+
+select 
+
+CUSIP,ProductType,IssuerName,IssueDate,SettlementDate,MaturityDate,InterestRateType,IncomePaymentType,
+
+                PrincipalAmount,
+
+                SettlementAmount,
+
+                InterestRate,
+
+				TimeToMaturity,
+				
+				MaxTimeToMaturity,
+
+				min(SettlementDate)  over (partition by cusip, MaxTimeToMaturity) as EstIssueDate
+
+
+from (
+
+SELECT CUSIP,ProductType,IssuerName,IssueDate,SettlementDate,MaturityDate,InterestRateType,IncomePaymentType,
 
                 PrincipalAmount,
 
@@ -39,13 +61,15 @@ GetDTCCData <- function() {
 
                        SUM([settlementamount]) AS SettlementAmount,
 
-                       AVG([interestrate])     AS InterestRate
+                       AVG([interestrate])     AS InterestRate      
 
              FROM   [NBDataHub].[DTCC].[CPCDtransactions]
 
          GROUP BY CUSIP,ProductType,IssuerName,IssueDate,SettlementDate,MaturityDate,InterestRateType,IncomePaymentType
 
-  ) AS dtcc"
+  ) AS dtcc
+
+  ) as dtcc2"
 
 
   dbhandle <- odbcDriverConnect('driver={SQL Server};SERVER=wm-x-s-31;database = NBDataHub;trusted_connection=true')
@@ -60,6 +84,8 @@ GetDTCCData <- function() {
            SettlementDate = anytime::anydate(SettlementDate),
            MaturityDate = anytime::anydate(MaturityDate),
            IssueDate = anytime::anydate(IssueDate),
+           EstIssueDate = anytime::anydate(EstIssueDate),
+           OriginalMaturity = as.double(MaturityDate - EstIssueDate),
            TimeToMaturity = as.double(MaturityDate - SettlementDate),
            Yield = InterestRate,
            Yield = if_else(InterestRateType == "F" & IncomePaymentType == "Z",
@@ -70,11 +96,11 @@ GetDTCCData <- function() {
            IssueDate = if_else(is.na(IssueDate), SettlementDate, IssueDate))
 
 
-  dtcc_data = dtcc_data %>%
-    group_by(CUSIP, MaxTimeToMaturity) %>%
-    mutate(EstIssueDate = min(SettlementDate)) %>%
-    ungroup() %>%
-    mutate(OriginalMaturity = as.double(MaturityDate - EstIssueDate))
+  # dtcc_data = dtcc_data %>%
+  #   group_by(CUSIP, MaxTimeToMaturity) %>%
+  #   mutate(EstIssueDate = min(SettlementDate)) %>%
+  #   ungroup() %>%
+  #   mutate(OriginalMaturity = as.double(MaturityDate - EstIssueDate))
 
 
   dtcc_data = dtcc_data %>%
